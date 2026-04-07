58a054d6736a98717b66057e4ef4dd07ae283761
aniTech-addict <kirti7666anand15@gmail.com>
2026-04-07 10:42:58 +0530

    feat: handle creation and insertion of AI Analyzer button in DOM

---
+++ scripts/content.js
@@ -0,0 +1,50 @@
+import {getAnalysis} from "util/getAnalysisResponse.js"
+
+function getSiblingNode () {
+
+    const buttons = Array.from(document.querySelectorAll("button.bg-green-s"));
+    if (!buttons) {
+        console.info("No Buttons found");
+        return;
+    }
+    const solutionButton = buttons.find((button) => {
+        return button.textContent === "Solution";
+    });
+    if (!solutionButton) {
+        return null;
+    }
+    return solutionButton;
+}
+
+function createAiAnalyzerButton () {
+    const aiAnalyzerButton = document.createElement("div");
+    aiAnalyzerButton.addEventListener('click', (e) => {
+        getAnalysis(getCodeFromBrowser());
+        console.log("Button clicked");
+    });
+    aiAnalyzerButton.className = "inline-flex items-center justify-center gap-2 text-sm font-medium px-3.5 py-1 bg-pink rounded-sd-md opacity-80 h";
+    aiAnalyzerButton.style.cursor = "pointer";
+    const analyzerIcon = document.createElement("img");
+    analyzerIcon.src = chrome.runtime.getURL("images/analyzer.png");
+    analyzerIcon.alt = "Analyzer";
+    analyzerIcon.style.width = "16px";
+    analyzerIcon.style.height = "16px";
+
+    const analyzerLabel = document.createElement("span");
+    analyzerLabel.textContent = "Ai Analyzer";
+
+    aiAnalyzerButton.append(analyzerIcon, analyzerLabel);
+    return aiAnalyzerButton; 
+}
+
+function appendAiAnalyzerButton () {
+    const sibling = getSiblingNode();
+    if (!sibling) {
+        console.info("No Solution button found, exiting");
+        return;
+    }
+    const aiAnalyzerButton = createAiAnalyzerButton();
+    sibling.parentNode.insertAdjacentElement("afterBegin", aiAnalyzerButton);
+}
+
+
