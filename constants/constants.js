f27688357b99038d60bab15568eb2cfc91b9cdcd
aniTech-addict <kirti7666anand15@gmail.com>
2026-09-17 11:48:32 +0530

    feat: add constants for models and default model configuration

---
+++ constants/constants.js
@@ -0,0 +1,10 @@
+export const MODELS = [
+  'nvidia/nemotron-3-nano-omni-30b-a3b-reasoning:free',
+  'inclusionai/ling-3.0-flash-vl:free',
+  'nex-agi/nex-n2.5-mini:free',
+  'poolside/laguna-xs-2.1:free'
+];
+
+export const DEFAULT_MODEL = MODELS[1];
+
+export const API_KEY = ""
